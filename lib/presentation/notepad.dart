f72ac96da026e433af011ac2e44daac9ebb2a0d3
Naveed kaimkhani <naveedkaimkhami@gmail.com>
2024-07-27 13:39:12 +0500

    notepad screen done

--- lib/presentation/notepad.dart
+++ lib/presentation/notepad.dart
@@ -1,8 +1,8 @@
-import 'dart:math';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:speech_therapy/presentation/widgets/auth_button.dart';
 import 'package:speech_therapy/style/images.dart';
@@ -14,113 +14,123 @@ class NotePad extends StatefulWidget {
 }
 
 class _NotePadState extends State<NotePad> {
-  final titleController = TextEditingController();
   final messageController = TextEditingController();
-  // String? date;
+  final FlutterTts _flutterTts = FlutterTts();
 
   @override
   void initState() {
     super.initState();
-    // Setting an initial date for display purposes
-    // date = DateTime.now().toString();
+    _flutterTts.setLanguage("ur-PK");
+    _flutterTts.setPitch(1);
+  }
+
+  Future<void> _speak(String text) async {
+    await _flutterTts.speak(text);
+  }
+
+  @override
+  void dispose() {
+    _flutterTts.stop();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Styling.lightBlue,
-      body: Column(
+      body: Stack(
         children: [
-          Container(
-            height: 160.h,
-            decoration: BoxDecoration(
-              color: Styling.darkBlue,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(34.r),
-                bottomRight: Radius.circular(34.r),
-              ),
-            ),
-            child: Center(
-                // child: Image.asset(Images.login_icon),
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(left: 12.w, top: 4.h),
-                  child: Text(
-                    'Start\nWriting\nToday',
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.pacifico(
-                      textStyle: TextStyle(
-                        fontSize: 20.sp,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
+          Column(
+            children: [
+              Container(
+                height: 160.h,
+                decoration: BoxDecoration(
+                  color: Styling.darkBlue,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(34.r),
+                    bottomRight: Radius.circular(34.r),
                   ),
                 ),
-                SvgPicture.asset(
-                  Images.write,
-                  height: 150.h,
-                ),
-              ],
-            )),
-          ),
-          SizedBox(height: 16.h),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.all(16.w),
-              child: Stack(
-                children: [
-                  Container(
-                    height: double.infinity,
-                    padding: EdgeInsets.all(20.w),
-                    decoration: BoxDecoration(
-                      color: Styling.darkBlue, // Brighter color for kids
-                      borderRadius: BorderRadius.circular(20.r),
-                    ),
-                    child: TextField(
-                      controller: messageController,
-                      keyboardType: TextInputType.multiline,
-                      maxLines: null,
-                      textInputAction: TextInputAction.newline,
-                      decoration: const InputDecoration(
-                        hintText: "Enter message....",
-                        border: InputBorder.none,
-                      ),
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize:
-                              20), // Changed text color for better visibility
-                    ),
-                  ),
-                  Positioned(
-                    right: -10.w, // Adjust the position to ensure overlap
-                    top: -10.h, // Adjust the position to ensure overlap
-                    child: Container(
-                      height: 100.h, // Adjust height as needed
-                      width: 100.w, // Adjust width as needed
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(
-                            20.r), // Match the text box radius
-                        child: Image.asset(
-                          Images.bird, // Replace with the actual image path
-                          fit: BoxFit.cover,
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 12.w, top: 4.h),
+                        child: Text(
+                          'Start\nWriting\nToday',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.pacifico(
+                            textStyle: TextStyle(
+                              fontSize: 20.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      SvgPicture.asset(
+                        Images.write,
+                        height: 150.h,
+                      ),
+                    ],
                   ),
-                ],
+                ),
+              ),
+              SizedBox(height: 16.h),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(16.w),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(20.w),
+                          decoration: BoxDecoration(
+                            color: Styling.darkBlue,
+                            borderRadius: BorderRadius.circular(20.r),
+                          ),
+                          child: SingleChildScrollView(
+                            child: TextField(
+                              controller: messageController,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                              cursorColor: Colors.white,
+                              maxLines: null,
+                              decoration: InputDecoration(
+                                hintText: "Start Writing...",
+                                hintStyle: TextStyle(color: Colors.white70),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              AuthButton(
+                text: "Convert",
+                func: () async {
+                  await _speak(messageController.text);
+                },
+                color: Styling.darkBlue,
+              ),
+            ],
+          ),
+          Positioned(
+            top: 160.h, // Adjust as needed
+            right: 8.w, // Adjust as needed
+            child: Hero(
+              tag: 'hero-tag',
+              child: Image.asset(
+                Images.bird, // Replace with your image path
+                width: 100.w, // Adjust size as needed
+                height: 100.h, // Adjust size as needed
               ),
             ),
           ),
-          AuthButton(
-            text: "Convert",
-            func: () {
-              // Add the conversion logic here
-            },
-            color: Styling.darkBlue,
-          )
         ],
       ),
     );
